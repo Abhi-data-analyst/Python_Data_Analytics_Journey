be38ec6d0a5a3c3746c7716c35520264719c5d31
Abhiram sharan mishra <mishraabhiram1@gmail.com>
2026-02-03 21:09:09 +0530

    Add files via upload

---
+++ elegibility.py
@@ -0,0 +1,6 @@
+age=int(input("Please enter your age as on your adhhar card:"))
+category=str(input("please mention pwd status:"))
+if age>=60 or category=="yes":
+    print("Elegible for 15% discont:")
+else:
+    print("sorry ")
